561f198dc3e008d11f7956078e0dc2a7befb0613
Ashley Giang <129787453+thigiang16@users.noreply.github.com>
2025-05-05 19:06:42 -0700

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,18 @@
+import unittest
+from main import add, subtract, mul, poweroftwo
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 2), 3)
+
+    def test_mul(self):
+        self.assertEqual(mul(3, 4), 12)
+
+    def test_poweroftwo(self):
+        self.assertEqual(poweroftwo(6), 36)
+
+if __name__ == '__main__':
+    unittest.main()
